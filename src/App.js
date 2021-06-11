876509005aa1f50b3ff98287c27a0f168eff1797
jwatts82 <wattscodes@gmail.com>
2021-06-10 22:09:26 -0700

    main nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-// import './App.scss'
 import './styles/App.scss'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+    <div className="header-home">
+      <div className="container">
+        <Navbar />
+        <div className="header-content">
+          <h1>
+            I Am Jessica The {/* <Typing> */}
+            <span
+              className="txt-type"
+              data-wait="3000"
+              data-words="[Full Stack Developer, iOS Developer]"
+            ></span>
+            {/* </Typing> */}
+          </h1>
+          <p className="lead">I specialize in JavaScript, React and Swift</p>
+          <a href="/" className="btn-light">
+            View My Work
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
