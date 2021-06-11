33c702ae09156a96458d329e7b9b905edeed8d18
jwatts82 <wattscodes@gmail.com>
2021-06-10 22:11:58 -0700

    initial navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react'
 
 export default class Navbar extends Component {
   render() {
-    return <div></div>
+    return
+    ;<nav className="main-nav">
+      <img src={logo} alt="My Portfolio" id="logo" />
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Contact</a>
+        </li>
+        <li>
+          <a href="/">Work</a>
+        </li>
+      </ul>
+    </nav>
   }
 }
