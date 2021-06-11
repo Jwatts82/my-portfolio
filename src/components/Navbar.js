c94adc7f74549fa42e34acf70619a6466f5cc3b5
jwatts82 <wattscodes@gmail.com>
2021-06-10 22:18:00 -0700

    correct syntax

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react'
+import logo from '../img/logo.png'
 
-export default class Navbar extends Component {
+class Navbar extends Component {
   render() {
-    return
-    ;<nav className="main-nav">
-      <img src={logo} alt="My Portfolio" id="logo" />
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">About</a>
-        </li>
-        <li>
-          <a href="/">Contact</a>
-        </li>
-        <li>
-          <a href="/">Work</a>
-        </li>
-      </ul>
-    </nav>
+    return (
+      <nav className="main-nav">
+        <img src={logo} alt="My Portfolio" id="logo" />
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+          <li>
+            <a href="/">Work</a>
+          </li>
+        </ul>
+      </nav>
+    )
   }
 }
+
+export default Navbar
